ecd46e653593cbb8d86c259154c05bcc85fad434
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-09-15 17:14:27 -0700

    Added Local Storage get and set functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,10 @@ timeblocksArr = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm
 init();
 createTimeblocks();
 colorTimeBlocks();
+allSaveBtns = document.querySelectorAll('.saveBtn');
+allTextareas = document.querySelectorAll('.ta')
+addEventListeners();
+getLocalStorage();
 
 // Initialize the page with Current Day
 function init() {
@@ -33,14 +37,14 @@ function createTimeblocks() {
 
         var textarea = document.createElement('textarea');
         textarea.setAttribute('type', 'text');
-        textarea.setAttribute('class', 'col-8');
-        textarea.setAttribute('id', 'textarea-' + [i]);
+        textarea.setAttribute('class', 'col-8 ta textarea-' + [i]);
+        textarea.setAttribute('id', [i]);
         inputGroup.append(textarea);
 
         var saveBtn = document.createElement('button');
         saveBtn.setAttribute('class', 'btn saveBtn col-2');
-        saveBtn.setAttribute('id', 'saveBtn-' + [i]);
-        saveBtn.innerHTML = '<i class="far fa-save"></i>'
+        saveBtn.setAttribute('id', [i]);
+        saveBtn.innerHTML = '<i class="far fa-save" id="' + [i] + '"></i>'
         inputGroup.append(saveBtn);
     }
 }
@@ -49,17 +53,44 @@ function createTimeblocks() {
 function colorTimeBlocks() {
     for (i = 0; i < timeblocksArr.length; i++) {
         if (currentTime === timeblocksArr[i]) {
-            var current = document.getElementById('textarea-' + [i]);
-            current.setAttribute('class', 'col-8 present');
+            var current = document.querySelector('.textarea-' + [i]);
+            current.setAttribute('class', 'col-8 present ta textarea-' + [i]);
             currentIndex = i
         }
     }
     for (i = currentIndex + 1; i <= timeblocksArr.length - 1; i++) {
-        var future = document.getElementById('textarea-' + [i]);
-        future.setAttribute('class', 'col-8 future');
+        var future = document.querySelector('.textarea-' + [i]);
+        future.setAttribute('class', 'col-8 future ta textarea-' + [i]);
     }
     for (i = currentIndex - 1; i >= 0; i--) {
-        var past = document.getElementById('textarea-' + [i]);
-        past.setAttribute('class', 'col-8 past');
+        var past = document.querySelector('.textarea-' + [i]);
+        past.setAttribute('class', 'col-8 past ta textarea-' + [i]);
+    }
+}
+
+// Adds an Event Listener to All Save Buttons
+function addEventListeners() {
+    for (i = 0; i < allSaveBtns.length; i++) {
+        allSaveBtns[i].addEventListener('click', saveEvent);
+    }
+}
+
+// Saves Textarea Value to Local Storage
+function saveEvent(event) {
+    buttonClicked = event.target.id;
+    for (i = 0; i < allTextareas.length; i++) {
+        var textareaID = allTextareas[i].id
+        if (textareaID === buttonClicked){
+            localStorage.setItem(timeblocksArr[i], allTextareas[i].value)
+        }
+    }
+}
+// Retrieve Textarea values from Local Storage
+function getLocalStorage() {
+    for (i = 0; i < timeblocksArr.length; i++) {
+        var storage = localStorage.getItem(timeblocksArr[i]);
+        if (storage != null) {
+            allTextareas[i].textContent = storage
+        }
     }
 }
